2973e664ed3bd0882c6df7fd9306ba226af2cf0e
22078802 <amaokyof03@gmail.com>
2026-04-20 10:17:26 +0000

    add all changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
